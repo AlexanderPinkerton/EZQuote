1925f82ca945a5f88413a8e77202c7e5a01df08b
Alexander Pinkerton <Alexpinkerton88@gmail.com>
2015-04-25 17:29:24 -0400

    service sending notifications successfully

--- src/com/example/finalproject/MarketSummaryActivity.java
+++ src/com/example/finalproject/MarketSummaryActivity.java
@@ -12,9 +12,14 @@ import java.util.List;
 import org.json.JSONException;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.SearchManager;
 import android.app.SearchableInfo;
+import android.app.TaskStackBuilder;
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -69,11 +74,13 @@ public class MarketSummaryActivity extends Activity implements NewsDelegate, Sto
 		//stockLister.updateSecurities(stocks);
 
 		
+		startService(new Intent(this, AlertService.class));
 		
 	}
 
 	
-	
+
+
 	
 
 	
